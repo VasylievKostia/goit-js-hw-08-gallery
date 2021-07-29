6e96ec6514fada6d3ef406de70c7ecf20c3bc0d2
VasylievKostia <vasylievk91@gmail.com>
2021-07-30 02:33:37 +0300

    js08

---
+++ src/app.js
@@ -0,0 +1,131 @@
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const galleryRef = document.querySelector('.js-gallery')
+const galeryItemsMarkup = createGalleryItemsMarkup(galleryItems)
+const modalRef = document.querySelector('.lightbox')
+const closeBtnRef = document.querySelector('[data-action="close-lightbox"]')
+const galleryLinkRef = document.querySelector('.gallery__link')
+const modalImageRef = document.querySelector('.lightbox__image')
+const modalOverlayRef = document.querySelector('.lightbox__overlay')
+galleryRef.insertAdjacentHTML('beforeend', galeryItemsMarkup)
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems.map(({preview, original,description}) => {
+      
+    return `
+      <li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${original}"
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+
+  }).join('');
+    
+}
+
+function onOpenModal(e) {
+ e.preventDefault()
+  window.addEventListener('keydown', onEscPress)
+  if (e.target.nodeName !== 'IMG') {
+    return
+  }
+  modalRef.classList.add('is-open')
+  modalImageRef.src = e.target.dataset.source;
+  modalImageRef.alt = e.target.alt;
+
+}
+
+function onCloseModal() {
+    window.removeEventListener('keydown', onEscPress)
+    modalRef.classList.remove('is-open')
+    modalImageRef.src = ""
+}
+
+
+function onBackdrop(event) {
+    if (event.target === event.currentTarget) {
+    modalRef.classList.remove('is-open')
+  }
+}
+
+function onEscPress(event) {
+  if (event.code === 'Escape') {
+     onCloseModal()
+  }
+}
+
+galleryRef.addEventListener('click', onOpenModal)
+closeBtnRef.addEventListener('click', onCloseModal)
+modalOverlayRef.addEventListener('click', onBackdrop)
